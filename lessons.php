e1592f48e7cd9e41bc1f29304c3d86e24eb50117
“Shubham” <shubhampattar2@gmail.com>
2020-04-22 16:41:20 +0530

    Updated for latest CSS

--- lessons.php
+++ lessons.php
@@ -13,23 +13,23 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?=$course?></title>
-	<link rel="stylesheet" type="text/css" href="./css/lessonsStyle.css">
+	<link rel="stylesheet" type="text/css" href="./css/vs_style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<form method="post">
-	<div class="lessonsContainer">
-		<div class="lessonsHeading" >
+	<div class="vs_lessonsContainer">
+		<div>
 			<h2><?=$course?></h2>
 			<h5>Course Status:</h5>
 			<h4>Course Contents</h4>
 		</div>
 
-		<div class="lessonsListContainer">
-			<table class="coursetbl">
-				<tr class="tblHead">
+		<div class="vs_lessonsListContainer">
+			<table class="vs_table">
+				<tr class="vs_tblHead">
 					<th colspan="2" >Lessions</th>
-					<th class="checkST">Status</th>
+					<th class="vs_checkST">Status</th>
 				</tr>
 				<?php 
 					$i=1;
@@ -37,9 +37,9 @@
 					{
 					
 				?>
-				<tr class="tblData">
-					<td class="slno"><?=$i?></td>
-					<td><a href="lesson.php?lessonID=<?=$value['ID']?>" class="lessonsTitle" title="<?=$value['lesson_title']?>"><?=$value['lesson_title']?></a>
+				<tr class="vs_data">
+					<td class="vs_slno"><?=$i?></td>
+					<td><a href="lesson.php?lessonID=<?=$value['ID']?>" class="vs_lessonsTitle" title="<?=$value['lesson_title']?>"><?=$value['lesson_title']?></a>
 					<td class="checkST"><span class="fa fa-check-square" style="font-size: 25px;" aria-hidden='true'></span></td>
 				</tr>
 				<?php
